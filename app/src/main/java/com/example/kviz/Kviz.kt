d84fc95d2b8419b35ff18b396d3034fb266ee8fc
Selma <mandzoselma@gmail.com>
2019-11-27 16:42:24 +0100

    dodano peto pitanje

--- app/src/main/java/com/example/kviz/Kviz.kt
+++ app/src/main/java/com/example/kviz/Kviz.kt
@@ -8,7 +8,9 @@ class Kviz {
     var pitanje2: PITANJE = PITANJE(textPitanja = "Sarajevo je glavni grad Španije? ", daLiJeTacno = false)
     var pitanje3: PITANJE = PITANJE(textPitanja = "London je glavni grad Engleske? ", daLiJeTacno = true)
     var pitanje4: PITANJE = PITANJE(textPitanja = "Zagreb je glavni grad Hrvatske? ", daLiJeTacno = true)
+    var pitanje5: PITANJE = PITANJE(textPitanja = "Tokio je glavni grad Japana? ", daLiJeTacno = true)
 
-    var pitanja: List<PITANJE> = listOf(pitanje1, pitanje2, pitanje3, pitanje4 )
+    var pitanja: List<PITANJE> = listOf(pitanje1, pitanje2, pitanje3, pitanje4, pitanje5 )
+    
 
 }
